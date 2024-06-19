d97857a179754e7ee7c46cf8ed35597e52dc7313
SantiDonatoAH <santivdonato@gmail.com>
2024-06-19 15:21:42 -0300

    LEFTARROWRIGHTARROW

---
+++ Assets/spawncomida.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class spawncomida : MonoBehaviour
+{
+
+    public GameObject Empty;
+    public GameObject[] comida;
+    private int currentIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (comida.Length > 0)
+        {
+            DeactivateAll();
+            comida[currentIndex].SetActive(true); 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            DeactivateAll();
+            currentIndex--;
+            if (currentIndex < 0)
+            {
+                currentIndex = comida.Length - 1; 
+            }
+            comida[currentIndex].SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            DeactivateAll();
+            currentIndex++;
+            if (currentIndex >= comida.Length)
+            {
+                currentIndex = 0; 
+            }
+            comida[currentIndex].SetActive(true);
+        }
+
+    }
+
+
+    public void DeactivateAll()
+    {
+        for (int i = 0; i < comida.Length; i++)
+        {
+            comida[i].SetActive(false);
+        }
+
+    }
+}
